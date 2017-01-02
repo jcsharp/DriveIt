6549bb6bd98546f299dec10d3cb8aaedb8893721
jcman <jc@manoli.net>
2017-01-02 13:08:54 +0100

    Moved agent and brain in separate files; use fixed learning rate.

---
+++ agent.py
@@ -0,0 +1,113 @@
+import numpy as np
+import math, random
+from brain import Brain
+from Memory import SumTreeMemory
+
+MAX_EPSILON = 1.0
+MIN_EPSILON = 0.1 # stay a bit curious even when getting old
+EXPLORATION_STOP = 3000000   # at this step epsilon will be MIN_EPSILON
+LAMBDA = - math.log(0.01) / EXPLORATION_STOP  # speed of decay
+
+MEMORY_CAPACITY = 5000000
+BATCH_SIZE_PER_SAMPLE = 8
+UPDATE_TARGET_FREQUENCY = 10000
+GAMMA = 0.995 # discount factor
+
+class Agent:
+    steps = 0
+    episodes = 0
+    epsilon = MAX_EPSILON
+    lastTargetUpdateStep = 0
+    episode = []
+
+    def __init__(self, stateCnt, actionCnt):
+        self.stateCnt = stateCnt
+        self.actionCnt = actionCnt
+        
+        self.brain = Brain(stateCnt, actionCnt)
+        self.memory = SumTreeMemory(MEMORY_CAPACITY)
+        
+    def act(self, s):
+        if random.random() < self.epsilon:
+            return random.randint(0, self.actionCnt - 1)
+        else:
+            return np.argmax(self.brain.predictOne(s))
+
+    def observe(self, sample):  # in (s, a, r, s_) format
+        self.episode.insert(0, sample)
+        self.steps += 1
+
+    def endEpisode(self):
+        n = len(self.episode)
+        if n == 0: return
+
+        # calculate Q_min values for the episode steps
+        q = 0
+        batch = []
+        for i in range(n):
+            s, a, r, s_ = self.episode[i]
+            q = r + GAMMA * q
+            batch.append((s, a, r, s_, q)) 
+        
+        # get the target model error on the steps for prioritized replay 
+        x, y, err = self._getTargets(batch)
+        
+        # add steps to memory, prioritized by error
+        for i in range(len(batch)):
+            self.memory.add(err[i], batch[i])
+        
+        self.episode = []
+        self.episodes += 1
+
+        # slowly decrease epsilon based on our eperience
+        self.epsilon = MIN_EPSILON \
+        + (MAX_EPSILON - MIN_EPSILON) * math.exp(-LAMBDA * self.steps)
+        
+        # memory replay based on the number of steps in the episode
+        self.replay(int(n * BATCH_SIZE_PER_SAMPLE))
+        
+    def _getTargets(self, batch):
+        no_state = np.zeros(self.stateCnt)
+        states = np.array([ o[0] for o in batch ], dtype=np.float32)
+        states_ = np.array([ (no_state if o[3] is None else o[3]) \
+                            for o in batch ], dtype=np.float32)
+
+        p = self.brain.predict(states)
+        p_ = self.brain.predict(states_, target = False)
+        pTarget_ = self.brain.predict(states_, target = True)
+        
+        x = np.zeros((len(batch), self.stateCnt)).astype(np.float32)
+        y = np.zeros((len(batch), self.actionCnt)).astype(np.float32)
+        err = np.zeros(len(batch))
+
+        for i in range(len(batch)):
+            s, a, r, s_, q = batch[i]
+           
+            t = p[i,0]
+            if s_ is None:
+                ta = r
+            else:
+                #ta = r + GAMMA * pTarget_[i,0][ np.argmax(p_[i,0]) ]  # double DQN
+                ta = max(q, r + GAMMA * pTarget_[i,0][ np.argmax(p_[i,0]) ]) # Q-min DDQN
+
+            err[i] = np.abs(t[a] - ta)
+            t[a] = ta
+            x[i] = s
+            y[i] = t
+            
+        return (x, y, err)
+        
+    def replay(self, size):
+        # eriodically update the target network
+        if self.steps - self.lastTargetUpdateStep >= UPDATE_TARGET_FREQUENCY:
+            self.brain.updateTargetModel()
+            self.lastTargetUpdateStep = self.steps
+        
+        # get samples batch and train over the targets
+        indexes, batch = self.memory.sample(size)
+        x, y, err = self._getTargets(batch)
+        self.brain.train(x, y)
+        
+        # update the batch steps errors in memory
+        for i in range(len(batch)):
+            self.memory.update(indexes[i], err[i])
---
+++ brain.py
@@ -0,0 +1,74 @@
+import numpy as np
+from cntk import *
+from cntk.models import Sequential
+from cntk.layers import *
+
+HL_SIZE = 128
+
+class Brain:
+    def __init__(self, stateCnt, actionCnt):
+        self.stateCnt = stateCnt
+        self.actionCnt = actionCnt
+        self._createModel()
+        self.updateTargetModel()
+        
+    def _createModel(self):
+        observation = input_variable(self.stateCnt, np.float32, name="s")
+        q_target = input_variable(self.actionCnt, np.float32, name="q")
+
+        l1 = Dense(HL_SIZE)
+        l2 = Dense(HL_SIZE, activation=relu)
+        #l3 = Dense(HL_SIZE, activation=relu)
+        #lo = Dense(self.actionCnt)
+        #q = Sequential([l1, l2, l3, lo])
+        
+        v1 = Dense(HL_SIZE, activation=relu)
+        vo = Dense(1)
+        value = Sequential([l1, l2, v1, vo])
+        
+        a1 = Dense(HL_SIZE, activation=relu)
+        ao = Dense(self.actionCnt)
+        advantage = Sequential([l1, l2, a1, ao])
+
+        q = plus(value, minus(advantage, reduce_mean(advantage, axis=0)))
+        
+        self.value = value(observation)
+        self.advantage = advantage(observation)
+        self.model = q(observation)
+
+        self.loss = reduce_mean(square(self.model - q_target), axis=0)
+        eval_function = reduce_mean(square(self.model - q_target), axis=0)
+
+        #     0.0  0.3  0.6   0.9   1.2   1.5
+        #lr = [0.5, 0.2, 0.1, 0.05, 0.02, 0.02]
+        #epoch_size = 300000 * 8
+        #lr_schedule = learning_rate_schedule(lr, UnitType.sample, epoch_size=epoch_size)
+        lr_schedule = learning_rate_schedule(0.2, UnitType.minibatch)
+        momentum = momentum_schedule(0.9)
+
+        learner = momentum_sgd(self.model.parameters, lr_schedule, momentum, \
+                               gradient_clipping_threshold_per_sample=10)
+        self.trainer = Trainer(self.model, self.loss, eval_function, learner)
+    
+    def train(self, x, y):
+        arguments = dict(zip(self.loss.arguments, [y,x]))
+        return self.trainer.train_minibatch(arguments, outputs=[self.loss.output])
+
+    def predict(self, s, target=False):
+        if target:
+            return self.model_.eval(s)
+        else:
+            return self.model.eval(s)
+
+    def predictOne(self, s, target=False):
+        return self.predict(s.reshape(1, self.stateCnt), target=target).flatten()
+
+    def updateTargetModel(self):
+        self.model_ = self.model.clone(method='clone')
+        
+    def saveModel(self, path):
+        self.model.save_model(path, False)
+        
+    def loadModel(self, path):
+        self.model = load_model(path)
+        self._createTrainer()
