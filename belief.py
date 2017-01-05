ae2288615dfa8d464054ad3b59e6bb4af4db2342
jcman <jc@manoli.net>
2017-01-05 10:14:33 +0100

    Visualize the coordinate transforms.

--- belief.py
+++ belief.py
@@ -6,10 +6,8 @@ class PositionTracking():
 
     def __init__(self, car=Car()):
         self.car = car
-        high = np.array([  1.,  1.,  pi, car.specs.v_max,  car.specs.K_max ])
-        low  = np.array([ -1., -1., -pi,             0.0, -car.specs.K_max ])
-        #high = np.array([  checkpoint_median_length,  half_track_width,  pi, car.specs.v_max,  car.specs.K_max, 1.0 ])
-        #low  = np.array([ -checkpoint_median_length, -half_track_width, -pi,             0.0, -car.specs.K_max, 0.0 ])
+        high = np.array([  checkpoint_median_length,  half_track_width,  pi, car.specs.v_max,  car.specs.K_max ])
+        low  = np.array([ -checkpoint_median_length, -half_track_width, -pi,             0.0, -car.specs.K_max ])
         self.action_space = spaces.Discrete(len(steer_actions))
         self.observation_space = spaces.Box(low, high)
 
@@ -17,17 +15,17 @@ class PositionTracking():
     def reset(self, observation):
         d, blue, theta, v, K, thres = observation
         #x, y = DriveItEnv.median_to_cartesian(d, 0.0)
-        #self.observation = observation
-        #self.position = x, y, d < 0.0
+        self.observation = observation
         x, y, theta = self.car.get_position()
-        return x, y, theta, v, K
+        self.position = x, y, d < 0.0
+        return d, 0., theta, v, K
 
 
     def update(self, observation, dt):
         #x_, y_, checkpoint = self.position
-        #d, blue, theta, v, K, thres = observation
+        d, blue, theta, v, K, checkpoint_passed = observation
         #d_, blue_, theta_, v_, K_, _ = self.observation
-        #self.observation = observation
+        self.observation = observation
 
         #a = (v - v_) / dt
         #K_dot = (K - K_) / dt
@@ -56,15 +54,13 @@ class PositionTracking():
 
         #y_m = DriveItEnv.lateral_error(x, y, x_m)
 
-        #self.position = (x, y, checkpoint)
-
         x, y, theta = self.car.get_position()
         steer, throttle, d, v, K = self.car.state
-        #x_m, y_m = DriveItEnv.cartesian_to_median(x, y, thres)
-        #x_m, _, _ = DriveItEnv.median_distance(x, y, d_)
-        #y_m = DriveItEnv.lateral_error(x, y, x_m)
+        x_m, y_m = DriveItEnv.cartesian_to_median(x, y, checkpoint_passed)
 
-        return x, y, theta, v, K
+        self.position = (x, y, checkpoint_passed)
+
+        return x_m, y_m, theta, v, K
 
 
     def normalize(self, belief):
