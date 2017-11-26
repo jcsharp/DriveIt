855d7dfc0e3998d7e85850791b5983774279de8c
jcman <jc@manoli.net>
2017-11-26 10:31:47 +0100

    Added openai ppo2 trainer.

---
+++ trainb.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+import sys
+import argparse
+from baselines import bench, logger
+from belief import BeliefDriveItEnv
+
+import numpy as np
+import tensorflow as tf
+from baselines.a2c.utils import conv, fc, conv_to_fc, batch_to_seq, seq_to_batch, lstm, lnlstm
+from baselines.common.distributions import make_pdtype
+
+class DriveItPolicy(object):
+
+    def __init__(self, sess, ob_space, ac_space, nbatch, nsteps, reuse=False): #pylint: disable=W0613
+        ob_shape = (nbatch,) + ob_space.shape
+        nact = ac_space.n
+        X = tf.placeholder(tf.float32, ob_shape, name='Ob') #obs
+        with tf.variable_scope("model", reuse=reuse):
+            h1 = fc(X, 'pi_fc1', nh=64, init_scale=np.sqrt(2), act=tf.tanh)
+            h2 = fc(h1, 'pi_fc2', nh=64, init_scale=np.sqrt(2), act=tf.tanh)
+            pi = fc(h2, 'pi', nact, act=lambda x:x, init_scale=0.01)
+            h1 = fc(X, 'vf_fc1', nh=64, init_scale=np.sqrt(2), act=tf.tanh)
+            h2 = fc(h1, 'vf_fc2', nh=64, init_scale=np.sqrt(2), act=tf.tanh)
+            vf = fc(h2, 'vf', 1, act=lambda x:x)[:,0]
+
+        self.pdtype = make_pdtype(ac_space)
+        self.pd = self.pdtype.pdfromflat(pi)
+
+        a0 = self.pd.sample()
+        neglogp0 = self.pd.neglogp(a0)
+        self.initial_state = None
+
+        def step(ob, *_args, **_kwargs):
+            a, v, neglogp = sess.run([a0, vf, neglogp0], {X:ob})
+            return a, v, self.initial_state, neglogp
+
+        def value(ob, *_args, **_kwargs):
+            return sess.run(vf, {X:ob})
+
+        self.X = X
+        self.pi = pi
+        self.vf = vf
+        self.step = step
+        self.value = value
+
+def train(num_timesteps, seed):
+    from baselines.common import set_global_seeds
+    from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
+    from baselines.common.vec_env.vec_frame_stack import VecFrameStack
+    from baselines.ppo2 import ppo2
+    import gym
+    import logging
+    import multiprocessing
+    import os.path as osp
+    ncpu = multiprocessing.cpu_count()
+    if sys.platform == 'darwin': ncpu //= 2
+    config = tf.ConfigProto(allow_soft_placement=True,
+                            intra_op_parallelism_threads=ncpu,
+                            inter_op_parallelism_threads=ncpu)
+    config.gpu_options.allow_growth = True #pylint: disable=E1101
+    gym.logger.setLevel(logging.WARN)
+    tf.Session(config=config).__enter__()
+
+    def make_env(rank):
+        def env_fn():
+            env = BeliefDriveItEnv(time_limit=180)
+            env.seed(seed + rank)
+            env = bench.Monitor(env, logger.get_dir() and osp.join(logger.get_dir(), str(rank)))
+            return env
+        return env_fn
+    nenvs = 8
+    env = SubprocVecEnv([make_env(i) for i in range(nenvs)])
+    set_global_seeds(seed)
+    env = VecFrameStack(env, 4)
+    policy = DriveItPolicy
+    ppo2.learn(policy=policy, env=env, nsteps=128, nminibatches=4,
+        lam=0.95, gamma=0.99, noptepochs=4, log_interval=1,
+        ent_coef=.01,
+        lr=lambda f : f * 2.5e-4,
+        cliprange=lambda f : f * 0.1,
+        total_timesteps=int(num_timesteps * 1.1))
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--seed', help='RNG seed', type=int, default=0)
+    parser.add_argument('--num-timesteps', type=int, default=int(1e6))
+    args = parser.parse_args()
+    log_dir = 'metrics/' + datetime.now().strftime('%Y%m%d%H%M%S')
+    logger.configure(dir=log_dir, format_strs=['stdout','tensorboard'])
+    train(num_timesteps=args.num_timesteps, seed=args.seed)
+
+
+if __name__ == '__main__':
+    main()
