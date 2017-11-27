e0fe71a80c5455cea8cf82647330983729eb2d08
jcman <jc@manoli.net>
2017-11-27 08:23:00 +0100

    Restore tf graph.

--- runppo2.py
+++ runppo2.py
@@ -2,30 +2,40 @@
 import sys
 import os.path as osp
 import argparse
-from datetime import datetime
+import tensorflow as tf
 from belief import BeliefDriveItEnv
 from policy import DriveItPolicy
 
 sys.path.append(osp.join(osp.dirname(osp.abspath(__file__)), "openai"))
-from baselines.ppo2 import Model
+from baselines.ppo2 import ppo2
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-t', '--num-timesteps', type=int, default=int(1e7))
     parser.add_argument('-l', '--log-dir', type=str, default='metrics')
-    parser.add_argument('-b', '--batch-name', type=str, default=datetime.now().strftime('%Y%m%d%H%M%S'))
-    parser.add_argument('-c', '--checkpoint', type=str)
+    parser.add_argument('-b', '--batch-name', type=str, default=None)
+    parser.add_argument('-f', '--file', type=str, default='model.meta')
     args = parser.parse_args()
 
-    checkpoint = osp.join(args.log_dir, args.batch_name, 'checkpoints', args.checkpoint)
-    print(checkpoint)
+    model_dir = osp.join(args.log_dir, args.batch_name)
+    model_file = osp.join(model_dir, args.file)
+    checkpoint_path = osp.join(model_dir, 'checkpoints', '00230')
+
     env = BeliefDriveItEnv(time_limit=180)
-    model = Model(DriveItPolicy, env.observation_space.shape, env.action_space.shape, 1, 1, 1, 0, 0, None)
-    model.load(checkpoint)
-    o = env.reset()
-    print(o)
-    a, v, s, neglogp = model.act_model.step(o)
-    print((a, v, s, neglogp))
+
+    loader = tf.train.import_meta_graph(model_file)
+    g = tf.get_default_graph()
+    pi = g.get_tensor_by_name("pi:0")
+
+    with tf.Session() as sess:
+        #print(tf.train.latest_checkpoint(checkpoint_path))
+        loader.restore(sess, checkpoint_path)
+
+        obs = env.reset()
+        print(obs)
+
+        act = sess.run(pi, feed_dict={'Ob:0': obs})
+        print(act)
 
 if __name__ == '__main__':
     main()
