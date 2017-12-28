8c807a80ea922ec87aa47cd572dfffb67bf1d32a
jcsharp <jc@manoli.net>
2017-12-28 19:02:48 +0100

    Run with variable speed and behavior.

--- runppo2.py
+++ runppo2.py
@@ -54,15 +54,15 @@ def render_one(model, time_limit, nbots, nenvs, nframes, seed, record=False):
     print((steps, reward, info))
     input('Done. Press enter to close.')
 
-pilots = (ReflexPilot,) # SharpPilot)
+pilots = (ReflexPilot, SharpPilot)
 bot_colors = [Color.orange, Color.purple, Color.navy]
 
 def create_env(time_limit, nbots, seed):
     cars = [Car.HighPerf(Color.green, v_max=2.0)]
     for i in range(nbots):
-        cars.append(Car.Simple(bot_colors[i], v_max=1.0))
+        cars.append(Car.Simple(bot_colors[i], v_max=1.2))
     bots = [pilots[i %  len(pilots)](cars[i], cars) for i in range(1, len(cars))]
-    env = BeliefDriveItEnv(cars[0], bots, time_limit, noisy=True, random_position=True, bot_speed_deviation=0.0)
+    env = BeliefDriveItEnv(cars[0], bots, time_limit, noisy=True, random_position=True, bot_speed_deviation=0.3)
     env.seed(seed)
     # env = bench.Monitor(env, logger.get_dir() and osp.join(logger.get_dir(), str(rank)))
     return env
@@ -102,7 +102,7 @@ def main():
     parser.add_argument('-b', '--batch-name', type=str, default=None)
     parser.add_argument('-m', '--model', type=str, default='make_model.pkl')
     parser.add_argument('-c', '--checkpoint', type=str)
-    parser.add_argument('-e', '--envs', help='number of environments', type=int, default=8)
+    parser.add_argument('-e', '--envs', help='number of environments', type=int, default=32)
     parser.add_argument('-f', '--frames', help='number of frames', type=int, default=4)
     parser.add_argument('-t', '--time-limit', type=int, default=180)
     parser.add_argument('-n', '--number-bots', type=int, default=2)
